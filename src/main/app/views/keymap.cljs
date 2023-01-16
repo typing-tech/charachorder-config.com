b3c320a9274b9f507675dd6eaecc82fec2da3a14
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-01-16 07:50:45 +0000

    working on code chooser com

--- src/main/app/views/keymap.cljs
+++ src/main/app/views/keymap.cljs
@@ -1,7 +1,26 @@
 (ns app.views.keymap
   (:require
-   [app.components :refer [button]]
+   [reagent.core :as r]
+   [app.components :refer [button popover]]
    [app.db :as db :refer [*db]]))
 
-(defn keymap-view [args]
-  )
+(defn code-table [port-id]
+  [:div {:class ""}
+   [:div {} "Tabs"]
+   [:div {} "Codes"]])
+
+(defn code-chooser-com []
+  (let []
+    (fn [port-id]
+      (let []
+        (popover
+         {:isOpen true
+          :positions ["top" "bottom" "right" "left"]
+          :align "start"
+          :reposition true
+          :content (r/as-element [code-table port-id])}
+         [:div {:class "pointer"} "click"])))))
+
+(defn keymap-view [{:keys [port-id]}]
+  (let []
+    [code-chooser-com port-id]))
