bfd628d30452f278fa93a8f6d5c274e95c6c4fff
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-01-20 05:14:17 +0000

    v.resets: initial commit

---
+++ src/main/app/views/resets.cljs
@@ -0,0 +1,13 @@
+(ns app.views.resets
+  (:require
+   [posh.reagent :as posh :refer [pull q]]
+   [app.db :as db :refer [*db]]
+   [app.codes :refer [var-params]]
+   [app.components :refer [button]]
+   [app.serial :as serial :refer [has-web-serial-api?
+                                  *ports]]))
+
+(defn resets-view [{:keys [port-id]}]
+  (let []
+    [:div {:class "pa3"}
+     (button #(serial/reset-keymaps! port-id) ["RESET keymaps and COMMIT"] :error true)]))
