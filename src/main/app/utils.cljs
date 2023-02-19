2353edeab69ffb534263e668757b00e25fc90947
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-02-19 21:17:14 +0000

    utils: new human time fns

--- src/main/app/utils.cljs
+++ src/main/app/utils.cljs
@@ -11,5 +11,9 @@
 (defn bimap [m]
   (merge m (set/map-invert m)))
 
-(defn human-time-now-with-seconds []
-  (date-fns/formatISO9075 (.now js/Date)))
+(defn timestamp-ms []
+  (.now js/Date))
+
+(defn human-time-with-seconds
+  ([] (human-time-with-seconds (timestamp-ms)))
+  ([t] (date-fns/formatISO9075 t)))
