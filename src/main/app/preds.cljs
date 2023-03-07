8cf9e3046b317e7d8fc2821a5f76462b6a7f8680
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-03-07 02:01:11 +0000

    fix read only mode being broken

--- src/main/app/preds.cljs
+++ src/main/app/preds.cljs
@@ -1,9 +1,14 @@
 (ns app.preds
   (:require
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [app.ratoms :refer [*url-search-params
+                       *num-devices-connected
+                       *active-port-id]]))
 
-(defn is-device-not-yet-determined? [{:keys [*device-name]}]
-  (= @*device-name "???"))
+(defn is-device-not-yet-determined? [{:as port :keys [*device-name]}]
+  (and port (= @*device-name "???")))
 
-(defn is-device-cc1? [{:keys [*device-name]}]
-  (str/starts-with? @*device-name "CHARACHORDER ONE "))
+(defn is-device-cc1? [{:as port :keys [*device-name]}]
+  (or (and (not port)
+           (.has @*url-search-params "cc1-layout"))
+      (str/starts-with? @*device-name "CHARACHORDER ONE ")))
