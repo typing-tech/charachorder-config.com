4f5e2f17d149629a35456b7949fabf3cb60eba5e
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-03-08 07:08:50 +0000

    preds: bug fix primary preds

--- src/main/app/preds.cljs
+++ src/main/app/preds.cljs
@@ -9,13 +9,11 @@
   (and port (= @*device-name "???")))
 
 (defn is-device-cc1? [{:as port :keys [*device-name]}]
-  (and (and (not port)
-            (not (.has @*url-search-params "cc-lite-layout")))
+  (and (or port (and (not port) (not (.has @*url-search-params "cc-lite-layout"))))
        (or (and (not port) (.has @*url-search-params "cc1-layout"))
            (str/starts-with? @*device-name "CHARACHORDER ONE "))))
 
 (defn is-device-cc-lite? [{:as port :keys [*device-name]}]
-  (and (and (not port)
-            (not (.has @*url-search-params "cc1-layout")))
+  (and (or port (and (not port) (not (.has @*url-search-params "cc1-layout"))))
        (or (and (not port) (.has @*url-search-params "cc-lite-layout"))
            (str/starts-with? @*device-name "CHARACHORDER LITE "))))
