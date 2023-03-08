f6ad5dbbf2e5bf2fc71c873261147d9fd7dfa5bf
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-03-08 20:37:56 +0000

    add baud rate for CC Lite M0

--- src/main/app/serial/constants.cljs
+++ src/main/app/serial/constants.cljs
@@ -1,6 +1,9 @@
 (ns app.serial.constants)
 
-(def baud-rates {[9114 32783] 115200})
+(def baud-rates {;; CC1 M0
+                 [9114 32783] 115200
+                 ;; CC Lite M0
+                 [9114 32796] 115200})
 (defonce *ports (atom {}))
 (def dummy-port-id "0")
 
