ea913e05392dc0ecb669ada60cf44cc80c55564a
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-03-08 06:05:41 +0000

    hw.cc-lite: add unit measurements

--- src/main/app/hw/cc_lite.cljs
+++ src/main/app/hw/cc_lite.cljs
@@ -3,19 +3,19 @@
 (def switch-keys
   {;; bottom row
    "lgui" {:location "0"}
-   "lctrl" {:location "1"}
-   "lalt" {:location "2"}
-   "lspace" {:location "3"}
+   "lctrl" {:location "1" :u 1.25}
+   "lalt" {:location "2" :u 1.25}
+   "lspace" {:location "3" :u 2}
    "lfn" {:location "4"}
    "ralt" {:location "5"}
-   "rspace" {:location "6"}
-   "rfn" {:location "7"}
-   "rgui" {:location "8"}
+   "rspace" {:location "6" :u 2}
+   "rfn" {:location "7" :u 1.25}
+   "rgui" {:location "8" :u 1.25}
    "left" {:location "9"}
    "down" {:location "10"}
    "right" {:location "11"}
    ;; zxc row
-   "lshift" {:location "12"}
+   "lshift" {:location "12" :u 2}
    "z" {:location "13"}
    "x" {:location "14"}
    "c" {:location "15"}
@@ -30,7 +30,7 @@
    "up" {:location "24"}
    "del" {:location "25"}
    ;; asd row
-   "capslock" {:location "26"}
+   "capslock" {:location "26" :u 1.75}
    "a" {:location "27"}
    "s" {:location "28"}
    "d" {:location "29"}
@@ -42,9 +42,9 @@
    "l" {:location "35"}
    ";" {:location "36"}
    "'" {:location "37"}
-   "enter" {:location "38"}
+   "enter" {:location "38" :u 2.25}
    ;; qwe row
-   "tab" {:location "39"}
+   "tab" {:location "39" :u 1.5}
    "q" {:location "40"}
    "w" {:location "41"}
    "e" {:location "42"}
@@ -57,7 +57,7 @@
    "p" {:location "49"}
    "[" {:location "50"}
    "]" {:location "51"}
-   "bslash" {:location "52" :u 1}
+   "bslash" {:location "52" :u 1.5}
    ;; number row
    "esc" {:location "53" :u 1}
    "1" {:location "54"}
@@ -72,7 +72,7 @@
    "0" {:location "63"}
    "-" {:location "64"}
    "=" {:location "65"}
-   "bs" {:location "66"}})
+   "bs" {:location "66" :u 2}})
 
 (def location->switch-key-id
   (into {} (map (fn [[k {:keys [location]}]] [location k])
