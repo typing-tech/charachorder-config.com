4d64e89c1d61356cea926275623d1e42b0b41671
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-03-11 17:32:03 +0000

    resets: add message to wait

--- src/main/app/views/resets.cljs
+++ src/main/app/views/resets.cljs
@@ -13,10 +13,15 @@
       (button #(ops/reset-restart! port-id) ["Restart Device"])
       (button #(ops/reset-bootloader! port-id) ["Enter Bootloader Mode"])]
      [:div.mb5
-      (button #(ops/reset-starter! port-id) ["Add Starter Chords"] :warning true)
+      (button #(ops/reset-starter! port-id) ["Add Starter Chords (WAIT 1 MINUTE after click)"]
+              :warning true)
       (button #(ops/reset-func! port-id) ["Add Functional Chords (BS, DEL, Arrow)"] :warning true)]
      [:div.mb5
       (button #(ops/reset-params! port-id) ["RESET Params and COMMIT"] :danger true)
-      (button #(ops/reset-keymaps! port-id) ["RESET keymaps and COMMIT"] :danger true)
-      (button #(ops/reset-clearcml! port-id) ["DELETE ALL Chords"] :danger true)
+      [:div.mv3]
+      (button #(ops/reset-keymaps! port-id) ["RESET keymaps and COMMIT "]
+              :danger true)
+      (button #(ops/reset-clearcml! port-id) ["DELETE ALL Chords (WAIT 1 MINUTE after click)"]
+              :danger true)
+      [:div.mv3]
       (button #(ops/factory-reset! port-id) ["FACTORY RESET"] :danger true)]]))
