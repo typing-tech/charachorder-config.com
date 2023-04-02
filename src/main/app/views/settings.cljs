f6b2ed18e07da56220e1f3f24c379fab9fb774aa
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-02 07:41:43 +0000

    v.settings: add advanced-params setting

--- src/main/app/views/settings.cljs
+++ src/main/app/views/settings.cljs
@@ -9,10 +9,20 @@
 (defn settings-view []
   (let [x @*force-rerender]
     [:div {:class "pa3 mb5" :data-dummy x}
+     ;; CC1 Compact Mode
      (let [cc1-compact-mode (settings/get :cc1-compact-mode false)]
        (button #(do (settings/set! :cc1-compact-mode (not cc1-compact-mode))
                     (swap! *force-rerender inc))
                (if cc1-compact-mode
                  ["Turn off CC1 Compact Layout"]
                  ["Switch to CC1 Compact Layout"])
-               :active cc1-compact-mode))]))
+               :active cc1-compact-mode))
+     ;; Advanced Parameters
+      (let [advanced-params (settings/get :advanced-params false)]
+        (button #(do (settings/set! :advanced-params (not advanced-params))
+                      (swap! *force-rerender inc))
+                (if advanced-params
+                  ["Turn off Advanced Parameters"]
+                  ["Turn on Advanced Parameters"])
+                :active advanced-params))
+     ]))
