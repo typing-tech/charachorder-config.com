83084486163e9a1a928af5c876bc81b187e370d0
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-02 07:40:48 +0000

    codes: add advanced, min, max, step, defaults, etc.

--- src/main/app/codes.cljs
+++ src/main/app/codes.cljs
@@ -21,99 +21,121 @@
    :set-keymap "B4"})
 
 (def var-params
-  {:enable-serial-header {:code "01" :type :num-boolean
+  {:enable-serial-header {:code "01" :type :num-boolean :advanced true
                           :label ["Serial Header"
                                   [:span.red " (breaks dot i/o)"]]}
-   :enable-serial-logging {:code "02" :type :num-boolean
+   :enable-serial-logging {:code "02" :type :num-boolean :advanced true
                            :label ["Serial Logging"
                                    [:span.red " (breaks dot i/o)"]]}
-   :enable-serial-debugging {:code "03" :type :num-boolean
+   :enable-serial-debugging {:code "03" :type :num-boolean :advanced true
                              :label ["Serial Debugging"
                                      [:span.red " (breaks dot i/o)"]]}
-   :enable-serial-raw {:code "04" :type :num-boolean
+   :enable-serial-raw {:code "04" :type :num-boolean :advanced true
                        :label ["Serial Raw"
                                [:span.red " (???)"]]}
-   :enable-serial-chord {:code "05" :type :num-boolean
+   :enable-serial-chord {:code "05" :type :num-boolean :advanced true
                          :label ["Serial Chord"]}
-   :enable-serial-keyboard {:code "06" :type :num-boolean
+   :enable-serial-keyboard {:code "06" :type :num-boolean :advanced true
                             :label ["Serial Keyboard"]}
-   :enable-serial-mouse {:code "07" :type :num-boolean
+   :enable-serial-mouse {:code "07" :type :num-boolean :advanced true
                          :label ["Serial Mouse"]}
-   :enable-usb-hid-keyboard {:code "11" :type :num-boolean
+   :enable-usb-hid-keyboard {:code "11" :type :num-boolean :advanced true
                              :label ["USB HID Keyboard"]}
    :enable-character-entry {:code "12" :type :num-boolean
                             :label ["Character Entry Mode"]}
 
-   :gui-ctrl-swap-mode {:code "13" :type :num-boolean :ccl-only true
+   :gui-ctrl-swap-mode {:code "13" :type :num-boolean :ccl-only true :advanced true
                         :label ["Swap GUI and Ctrl" [:br]
                                 [:span.white " (CC Lite only)"]]}
 
-   :key-scan-duration {:code "14" :type :ms
-                       :label ["Key Scan Duration"]}
-   :key-debounce-press-duration {:code "15" :type :ms
-                                 :label ["Key Debounce Press Duration"]}
-   :key-debounce-release-duration {:code "16" :type :ms
-                                   :label ["Key Debounce Release Duration"]}
-   :keyboard-output-character-microsecond-delays {:code "17" :type :us
-                                                  :label ["Character Output Delay (μs)"]}
+   :key-scan-duration {:code "14" :type :ms :min 1 :max 20 :step 1
+                       :defaults [["Universal: 2ms" 2]]
+                       :label ["Key Scan Duration (ms)"]}
+   :key-debounce-press-duration {:code "15" :type :ms :min 0 :max 50 :step 1
+                                 :defaults [["CC1: 7ms" 7]
+                                            ["CC Lite: 20ms" 20]]
+                                 :label ["Key Debounce Press Duration (ms)"]}
+   :key-debounce-release-duration {:code "16" :type :ms :min 0 :max 50 :step 1
+                                   :defaults [["CC1: 7ms" 7]
+                                              ["CC Lite: 20ms" 20]]
+                                   :label ["Key Debounce Release Duration (ms)"]}
+   :keyboard-output-character-microsecond-delays
+   {:code "17" :type :us :min 0 :max 10240 :step 40
+    :defaults [["Universal: 480μs" 480]]
+    :label ["Character Output Delay (μs)"]}
 
-   :enable-usb-hid-mouse {:code "21" :type :num-boolean
+   :enable-usb-hid-mouse {:code "21" :type :num-boolean :advanced true
                           :label ["USB HID Mouse"]}
-   :slow-mouse-speed {:code "22" :type :mouse
-                      :label ["Slow Mouse Speed"]}
-   :fast-mouse-speed {:code "23" :type :mouse
-                      :label ["Fast Mouse Speed"]}
+   :slow-mouse-speed {:code "22" :type :mouse :min 1 :max 100 :step 1
+                      :defaults [["Universal: 5" 5]]
+                      :label ["Slow Mouse Speed" [:br] [:span.gray "(1 unit = 50px)"]]}
+   :fast-mouse-speed {:code "23" :type :mouse :min 1 :max 200 :step 1
+                      :defaults [["Universal: 25" 25]]
+                      :label ["Fast Mouse Speed" [:br] [:span.gray "(1 unit = 50px)"]]}
    :enable-active-mouse {:code "24" :type :num-boolean
                          :label ["Active Mouse" [:br] [:span.gray "(wiggle every minute)"]]}
-   :mouse-scroll-speed {:code "25" :type :pos-int
+   :mouse-scroll-speed {:code "25" :type :pos-int :min 1 :max 20 :step 1
+                        :defaults [["Universal: 1" 1]]
                         :label ["Mouse Scroll Speed"]}
-   :mouse-poll-duration {:code "26" :type :ms
-                         :label ["Mouse Poll Duration"]}
+   :mouse-poll-duration {:code "26" :type :ms :min 1 :max 100 :step 1
+                         :defaults [["Universal: 20ms" 20]]
+                         :label ["Mouse Poll Duration (ms)"]}
 
    :enable-chording {:code "31" :type :num-boolean
                      :label ["Chording"]}
    :enable-chording-character-counter-timeout
    {:code "32" :type :num-boolean
     :label ["Chording Character Counter Timeout"]}
-   :chording-character-counter-timeout-timer 
-   {:code "33" :type :ds
-    :label ["Chording Character Counter Timeout (deciseconds)"]}
-   :chord-detection-press-tolerance {:code "34" :type :ms
-                                     :label ["Chord Detection Press Tolerance"]}
-   :chord-detection-release-tolerance {:code "35" :type :ms
-                                       :label ["Chord Detection Release Tolerance"]}
+   :chording-character-counter-timeout-timer
+   {:code "33" :type :ds :min 0 :max 255 :step 1
+    :defaults [["Universal: 40ds" 40]]
+    :label ["Chording Character Counter Timeout" [:br] "(deciseconds)"]}
+   :chord-detection-press-tolerance {:code "34" :type :ms :min 1 :max 50 :step 1
+                                     :defaults [["Universal: 25ms" 25]]
+                                     :label ["Chord Detection Press Tolerance (ms)"]}
+   :chord-detection-release-tolerance {:code "35" :type :ms :min 1 :max 50 :step 1
+                                       :defaults [["Universal: 25ms" 25]]
+                                       :label ["Chord Detection Release Tolerance (ms)"]}
 
    :enable-spurring {:code "41" :type :num-boolean
                      :label ["Spurring"]}
-   :enable-spurring-character-counter-timeout {:code "42" :type :num-boolean
-                                               :label ["Spurring Character Counter Timeout"]}
-   :spurring-character-counter-timeout-timer {:code "43" :type :s
-                                              :label ["Spurring Character Counter Timeout (seconds)"]}
+   :enable-spurring-character-counter-timeout
+   {:code "42" :type :num-boolean
+    :label ["Spurring Character Counter Timeout"]}
+   :spurring-character-counter-timeout-timer
+   {:code "43" :type :s :min 0 :max 240 :step 1
+    :defaults [["Universal: 240s" 240]]
+    :label ["Spurring Character Counter Timeout" [:br]
+            "(seconds)"]}
 
    :enable-arpeggiates {:code "51" :type :num-boolean
                         :label ["Arpeggiates"]}
-   :arpeggiate-tolerance {:code "54" :type :ms
-                          :label ["Arpeggiate Tolerance"]}
+   :arpeggiate-tolerance {:code "54" :type :ms :min 100 :max 2000 :step 100
+                          :defaults [["Universal: 800ms" 800]]
+                          :label ["Arpeggiate Timeout"]}
 
    :enable-compound-chording {:code "61" :type :num-boolean
                               :label ["Compound Chording"]}
-   :compound-tolerance {:code "64" :type :ms
+   :compound-tolerance {:code "64" :type :ms :min 100 :max 5000 :step 100
+                        :defaults [["Universal: 1500ms" 1500]]
                         :label ["Compound Tolerance"]}
 
    :led-brightness {:code "81" :ccl-only true
-                    :type :non-neg-int :max 50
+                    :type :non-neg-int :min 0 :max 50 :step 1
+                    :defaults [["CC Lite: 5" 5]]
                     :label ["LED Brightness" [:br]
-                            [:span.white " (CC Lite only)"]]}
+                            [:span.white " (CC Lite only)"] [:br]
+                            [:span.gray " (> 5 may use too much power)"]]}
    :led-color-code {:code "82" :ccl-only true
-                    :type :non-neg-int :disabled true
+                    :type :non-neg-int
                     :label ["LED Color Code" [:br]
                             [:span.white " (CC Lite only)"]]}
    :enable-led-key-highlight {:code "83" :ccl-only true
-                              :type :num-boolean :disabled true
+                              :type :num-boolean
                               :label ["LED Key Highlight" [:br]
                                       [:span.white " (CC Lite only)"]]}
 
-   :operating-system {:code "91" :type :non-neg-int
+   :operating-system {:code "91" :type :dropdown
                       :values (bimap
                                {"Windows" "0"
                                 "Mac" "1"
