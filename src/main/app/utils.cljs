fa287b4b7740150a333988e4c000e9d930c6da96
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-13 06:17:59 +0000

    add parse-binary-chord-string

--- src/main/app/utils.cljs
+++ src/main/app/utils.cljs
@@ -1,8 +1,9 @@
 (ns app.utils
-  (:require
-   ["date-fns" :as date-fns]
-   [clojure.set :as set]
-   [cljs.cache :as cache]))
+  (:require ["date-fns" :as date-fns]
+            [app.codes :refer [code-int->keymap-code]]
+            [app.macros :refer-macros [cond-xlet ->hash]]
+            [cljs.cache :as cache]
+            [clojure.set :as set]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -11,6 +12,8 @@
 
 (defn bimap [m]
   (merge m (set/map-invert m)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn timestamp-ms []
   (.now js/Date))
@@ -24,3 +27,22 @@
    (swap! *human-time-cache
           #(cache/through human-time-with-seconds* % t))
    (get @*human-time-cache t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn binary->decimal [b]
+  (js/parseInt b 2))
+
+(defn parse-binary-chord-string
+  "`chunks` is a vector of 12 action codes, each 10 bits long, represented as an integer."
+  [full-bcs]
+  (cond-xlet
+   :do (assert (string? full-bcs))
+   :do (assert (= 128 (count full-bcs)))
+   :let [unused-binary-string (subs full-bcs 0 8)
+         bcs (subs full-bcs 8)]
+   :do (assert (= 120 (count bcs)))
+   :let [bs (vec (for [i (range 12)]
+                   (subs bcs (* 10 i) (* 10 (+ i 1)))))
+         chunks (mapv binary->decimal bs)]
+   :return (->hash unused-binary-string chunks)))
