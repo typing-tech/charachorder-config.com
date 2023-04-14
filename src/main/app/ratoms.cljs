ba91f60beacdf9be72d5859bb5021a8d4ee8ce80
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-14 15:29:42 +0000

    ratoms: remove default value for *current-tab-view, so saved state is used

--- src/main/app/ratoms.cljs
+++ src/main/app/ratoms.cljs
@@ -8,4 +8,4 @@
 (defonce *nav-expanded (r/atom false))
 (defonce *num-devices-connected (r/atom 0))
 (defonce *active-port-id (r/atom nil))
-(defonce *current-tab-view (r/atom :keymap))
+(defonce *current-tab-view (r/atom nil))
