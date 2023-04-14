6a2d4611862fbaf2f89cfbd400231c00cc07df11
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-14 15:33:53 +0000

    v.chords: [WIP] add chords table

--- src/main/app/views/chords.cljs
+++ src/main/app/views/chords.cljs
@@ -1,23 +1,62 @@
 (ns app.views.chords 
   (:require [app.codes :refer [code-int->short-dom]]
+            [app.db :refer [*db]]
             [app.macros :refer-macros [->hash cond-xlet]]
             [app.serial.constants :refer [get-port]]
-            [app.utils :refer [parse-binary-chord-string]]))
+            [app.utils :refer [hex-str->bin-str parse-binary-chord-string]]
+            [posh.reagent :as posh]))
 
 (defn chord-chunks-com [chunks]
   (let [chunks (filter #(not= 0 %) chunks)]
-    [:div {:class "chord-chunks mh2"}
-     (for [chunk chunks] 
-       [:div {:class "chord-chunks__chunk"} (code-int->short-dom chunk)])]))
+    (into
+     [:div {:class "chord-chunks mh2"}]
+     (for [chunk chunks]
+       (-> (code-int->short-dom chunk)
+           (assoc-in [1 :class] "chord-chunks__chunk"))))))
+
+(defn get-chords [port-id]
+  (->> @(posh/q '[:find ?e ?index ?hex-chord-string ?phrase
+                  :in $ ?port-id
+                  :where
+                  [?e :chord/port-id ?port-id]
+                  [?e :chord/index ?index]
+                  [?e :chord/hex-chord-string ?hex-chord-string]
+                  [?e :chord/phrase ?phrase]]
+                *db port-id)
+       (map #(zipmap [:e :index :hex-chord-string :phrase] %))
+       (sort-by :index)))
+
+(defn chords-table [chords]
+  [:table {:class "pure-table pure-table-horizontal"}
+   [:thead
+    [:tr
+     [:th "Index"]
+    ;;  [:th "Hex Chord String"]
+    ;;  [:th "Chunks"]
+     [:th "Chord"]
+     [:th "Phrase"]]]
+   [:tbody
+    (for [{:keys [e index hex-chord-string phrase]} chords]
+      (let [chunks (->> hex-chord-string
+                        hex-str->bin-str
+                        parse-binary-chord-string
+                        :chunks)] 
+        [:tr {:key e}
+        [:td index]
+        ;; [:td hex-chord-string]
+        ;; [:td (pr-str chunks)]
+        [:td [chord-chunks-com chunks]]
+        [:td phrase]]))]])
 
 (defn chords-view [{:as args :keys [port-id]}]
   (let [port (get-port port-id)
         {:keys [*binary-chord-string]} port
         binary-chord-string @*binary-chord-string
         {:keys [chunks]} (when binary-chord-string
-                           (parse-binary-chord-string binary-chord-string))]
+                           (parse-binary-chord-string binary-chord-string))
+        chords (get-chords port-id)]
     [:div {:class "pa3 pb0"}
-     [:div.f2
+     [:div.f2.mb3
       [:div.dib "Last Chord: "]
       [chord-chunks-com chunks]]
-     ]))
+     [chords-table chords]]))
