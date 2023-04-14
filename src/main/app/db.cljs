394865aa7a4c129129e5fe4275fffd8c69843358
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-14 15:14:35 +0000

    db: add chord entity attributes schema

--- src/main/app/db.cljs
+++ src/main/app/db.cljs
@@ -5,7 +5,12 @@
    ))
 
 (def main-db-schema
-  {:port/id {:db/cardinality :db.cardinality/one :db/unique :db.unique/identity}})
+  {:port/id {:db/cardinality :db.cardinality/one
+             :db/unique :db.unique/identity}
+   :chord/id {:db/cardinality :db.cardinality/one
+              :db/unique :db.unique/identity}
+   :chord/port {:db/cardinality :db.cardinality/one
+                :db/valueType :db.type/ref}})
 
 (defonce *db (ds/create-conn main-db-schema))
 
